41d899a953fdcb4517692f39e3afc7971a1812aa
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-09-11 15:57:36 -0700

    Practicing x86 assembly skills
    I've only done a bit of x86 assembly before and it was a few years ago. I have done MIPS though so I'm trying to learn how to do the same here

--- sarcasm.s
+++ sarcasm.s
@@ -5,16 +5,29 @@ global start
 
 section .text
 _main:
+	mov rax, 25
 label:
-	mov rcx, 0x21
+	push byte 0x21
 	call putchar
+
+	add rax, -1
+
+	cmp rax, 0
+	jne label
 	
 	mov rax, 0x2000001 ; exit
 	mov rdi, 0
 	syscall
 	
 
-putchar:
+putchar: ; clears rdx and rcx
+	pop word rdx ; pop return address
+	pop byte rcx ; pop argument
+	push word rdx ; push return address
+	push rax
+	push rdi
+	push rdx
+	push rsi
 	mov rax, 0x2000004 ; write
 	mov rdi, 1 ; stdout
 	push byte rcx
@@ -22,4 +35,8 @@ putchar:
 	mov rdx, 1
 	syscall
 	pop byte rcx
+	pop rsi
+	pop rdx
+	pop rdi
+	pop rax
 	ret
