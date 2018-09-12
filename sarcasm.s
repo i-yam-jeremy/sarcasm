5354c29ddc697899928576c6c4053b9e1112fb5f
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-09-12 09:38:28 -0700

    Wrote to and read from global buffer

--- sarcasm.s
+++ sarcasm.s
@@ -1,23 +1,28 @@
 global _main
 
-global start
-
+section .data
+	virtual_stack dd 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 section .text
+default rel
 _main:
-	mov rax, 25
-label:
-	push byte 0x21
+	mov dword [virtual_stack], 0x22
+	mov rcx, [virtual_stack]
+	push byte rcx
 	call putchar
 
+	add rbx, 1
 	add rax, -1
 
-	cmp rax, 0
-	jne label
+	push byte 0xA
+	call putchar
+
 	
 	mov rax, 0x2000001 ; exit
 	mov rdi, 0
 	syscall
+
+; TODO global variable that is large array that can be used for the stack and a separate stack "pointer" that is the index in the array (or allocate space on stack for this virtual stack by adding to the stack pointer)
 	
 
 putchar: ; clears rdx and rcx
