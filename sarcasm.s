a329556ce9ceb06e2c5eddaa11416ea1b1e05ad4
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-09-12 13:19:17 -0700

    Reads all of STDIN
    Reads all of STDIN until it reaches end of file (well actually until read syscall errors but they result in the same thing if input is redirected to stdin by using "<" on shell)

--- sarcasm.s
+++ sarcasm.s
@@ -7,43 +7,64 @@ section .data
 section .text
 default rel
 _main:
-	mov rcx, 0x21
-	call pushstack
-	mov rcx, 0x22
-	call pushstack
-
-	xor rcx, rcx ; clear rcx
-	
-	call popstack
+	mov rax, 25
+label:
+	call readchar
 	call putchar
-	call popstack
-	call putchar
+	jmp label
 
-	mov rcx, 0xA
-	call putchar
-
-	
 	mov rax, 0x2000001 ; exit
 	mov rdi, 0
 	syscall
 
 pushstack: ; takes argument in rcx
+	push rax
+	push rbx
 	mov rax, [stack_pos]
 	mov rbx, stack
 	mov [rbx + 8*rax], rcx
 	add rax, 1
 	mov [stack_pos], rax
+	pop rbx
+	pop rax
 	ret
 
 popstack: ; returns result in rcx
+	push rax
+	push rbx
+
 	mov rax, [stack_pos]
 	add rax, -1
 	mov rbx, stack
 	mov rcx, [rbx + 8*rax]
 	mov [stack_pos], rax
+
+	pop rbx
+	pop rax
+	ret
+
+readchar: ; returns result in rcx
+	push rax
+	push rdx
+
+	mov rax, 0x2000003 ; read
+	mov rdi, 0
+	push rcx ; value is ignored, just location on stack is necessary
+	mov rsi, rsp
+	mov rdx, 1
+	syscall
+	cmp rax, 0
+	je error
+
+	pop rcx
+
+	pop rdx
+	pop rax
 	ret
 
 putchar: ; takes argument in rcx
+	push rax
+	push rdx
 	mov rax, 0x2000004 ; write
 	mov rdi, 1 ; stdout
 	push rcx
@@ -51,4 +72,11 @@ putchar: ; takes argument in rcx
 	mov rdx, 1
 	syscall
 	pop rcx
+	pop rdx
+	pop rax
 	ret
+
+error:	
+	mov rax, 0x2000001 ; exit
+	mov rdi, 17
+	syscall
